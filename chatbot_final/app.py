88f5da507e0fb84b0fae28b1898cbd99657c35a3
Shruti H. <167879533+shruti-harayan@users.noreply.github.com>
2025-04-14 17:53:16 +0530

    Update app.py

--- chatbot_final/app.py
+++ chatbot_final/app.py
@@ -46,6 +46,5 @@ def chatbot():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     
